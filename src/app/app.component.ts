197b93bc1c9917be8477229aa8855c0e74c18fe6
VinayakBorhade <vinayakborhade@gmail.com>
2020-08-28 16:19:25 +0530

    custom component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,20 +22,20 @@ function log2(className) {
     console.log("log2 className ", className);
 }
 
-function log3(className) {
+// function log3(className) {
     
-    return (...args) => {
-        console.log("Arguments passed to this class's constructor are ", args);
-        return new className(...args);
-    };
-}
+//     return (...args) => {
+//         console.log("Arguments passed to this class's constructor are ", args);
+//         return new className(...args);
+//     };
+// }
 
-@log3
-class c1 {
-    constructor(arg1, arg2) {
-        console.log("constructor fired!!");
-    }
-}
+// @log3
+// class c1 {
+//     constructor(arg1, arg2) {
+//         console.log("constructor fired!!");
+//     }
+// }
 
 // const obj = new c1(5, 10);
 
